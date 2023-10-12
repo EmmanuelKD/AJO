6c4032127541225dd800b9ecd2662a115f9a2079
kemmanuel662@gmail.com <EmmanuelKD>
2023-10-12 14:33:59 +0000

    types added

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,107 +1,122 @@
-
-"use client"
-
+"use client";
 
 type AuthUserType = {
-    email: string,
-    password: string
-}
+  email: string;
+  password: string;
+};
 
 type entityType = {
-    created_at?: Date;
-    deleted_at?: Date;
-    updated_at?: Date;
-    objectId?: string;
+  created_at?: Date;
+  deleted_at?: Date;
+  updated_at?: Date;
+  objectId?: string;
 };
 
-
-
-type AppUser = entityType & {
-    email: string;
-    fname: string;
-    lname: string;
-    phoneNumber: string;
-    isVerified: boolean;
-    photoRef?: string;
-    productLiked?:string[],
-    productInCart?:string[],
-    purchased?:ProductPurchases[],
+type AjoUser = entityType & {
+  email: string;
+  fname: string;
+  lname: string;
+  phoneNumber: string;
+  isVerified: boolean;
+  followerCount: number;
+  followingCount: number;
+  photoRef?: string;
+  productLiked?: string[];
+  productInCart?: string[];
+  purchased?: ProductPurchases[];
 };
 
+type AjoProduct = entityType & ProductType;
 
-type AppProduct = entityType & ProductType;
-
-type ColorType={name:string,hex:string}
+type ColorType = { name: string; hex: string };
 
 type ProductType = {
-    id: string;
-    brand: Brand;
-    description?: string;
-    imageSrc: string;
-    colors?: ColorType[];
-    otherImages?: string[];
-    imageAlt: string;
-    href: string;
-    name: string;
-    rating: AppRatings[];
-    reviewCount: number;
-    price: number;
-    
-  };
+  id: string;
+  brand?: Brand;
+  brandId: string;
+  description?: string;
+  imageSrc: string;
+  colors?: ColorType[];
+  otherImages?: string[];
+  imageAlt: string;
+  href: string;
+  name: string;
+  rating: AjoRatings[];
+  reviewCount: number;
+  price: number;
+};
 
- type TestimonialType = {
-    id: string;
-    attribution: string;
-    quote: string;
-    img?: string;
-    name?: string;
-    // productId:string;
-  };
+type TestimonialType = {
+  id: string;
+  attribution: string;
+  quote: string;
+  img?: string;
+  name?: string;
+};
 
-  type ProductReviewType =entityType & {
-    id: string;
-    by: string;
-    byImgRef: string;
-    quote: string;
-    productId:string;
-  };
-  
-  
-  type AppRatings =entityType & {
-    id: string;
-    by: string;
-    rate: number; 
-    productId:string;
-  };
-  
-  type ProductPurchases=entityType&{
-    productId:string;
-    isReviewed:false;
-    dialogCancleCount:number;
-  };
+type ProductReviewType = entityType & {
+  id: string;
+  by: string;
+  byImgRef: string;
+  quote: string;
+  productId: string;
+};
 
-  type Post =entityType & {
-      media: string[],
-      caption: string,
-      userId: string,
-  };
+type AjoRatings = entityType & {
+  by?: Owner;
+  rate: number;
+  productId: string;
+};
 
-  type Brand =entityType & {
-      brandName: string, 
-      description: string,
-      followerCount: number,
-      followingCount: number,
-      profile: string,
-  };
+type ProductPurchases = entityType & {
+  productId: string;
+  isReviewed: false;
+  dialogCancleCount: number;
+};
 
-  type brandowner  = {
-    id: string,
-    profile: string, 
-  };
+type Post = entityType & {
+  media: Media[];
+  caption: string;
+  userId: string;
+  by: PostOwner;
+};
 
-  type media =entityType & {
-    url: string,
-    thumbnail: string,
+type PostOwner = Owner & {};
 
-  };
+type Brand = entityType & {
+  brandName: string;
+  description: string;
+  followerCount: number;
+  followingCount: number;
+  owner: BrandOwner;
+  associateOwners: BrandOwner[];
+};
 
+type Roles = "admin" | "ambassadors";
+
+type BrandOwner = Owner & {
+  role?: Roles;
+};
+
+type Owner = entityType & {
+  imageSrc: string;
+  usersName: string;
+  usersId: string;
+  type: "user" | "brand";
+};
+
+type Media = entityType & {
+  url: string;
+  thumbnail: string;
+  type: "video" | "image";
+};
+
+type ActionOwner = Owner & {};
+type UserAction = {
+  actionBy?: ActionOwner;
+  actionTo?: ActionOwner;
+  actionById: string;
+  actionToId: string;
+  actionDate: Date;
+  type: "like" | "comment" | "follow";
+};
