4c80756faf06cc7001c6e201077ea6d64669baea
kemmanuel662@gmail.com <EmmanuelKD>
2023-10-12 19:54:31 +0000

    main layout created

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,9 @@
-import ActiveBrand from "@/components/ajo-custom/active-brand";
-import ActiveCreate from "@/components/ajo-custom/active-create";
-import AjoCoin from "@/components/ajo-custom/ajo-coin";
+import MainLayout from "./layout/main-layout";
 
+ 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <ActiveBrand />
-      <ActiveCreate />
-      <AjoCoin/>
-    </main>
+    <MainLayout />
+ 
   );
 }
