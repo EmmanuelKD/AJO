6db07f807f6ee9a504d0cdcbf7177d1af3ba51f0
JAS0047 <sebastianjacin24@gmail.com>
2023-10-12 15:06:34 +0000

    The Pages for AJO

---
+++ src/app/pages/brand-home-feed/page.tsx
@@ -0,0 +1,10 @@
+'use client'
+
+
+export default function BrandHomeFeed() {
+    return (
+        <div>
+            timeline   page
+        </div>
+    );
+}
---
+++ src/app/pages/timeline/page.tsx
@@ -0,0 +1,11 @@
+'use client'
+ 
+ 
+export default function TimelinePage() {
+  return (
+    <div>
+    timeline   page
+    </div>
+  );
+}
+ 
