d17d5419f330e60772b3cda05d53493054128abe
woodfur <138519610+woodfur@users.noreply.github.com>
2023-10-13 13:37:01 +0000

    Update main-layout.tsx

--- src/app/layout/main-layout.tsx
+++ src/app/layout/main-layout.tsx
@@ -80,10 +80,7 @@ function Drawer({
                           : "text-gray-400 hover:text-white hover:bg-HoverColor"
                       } group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold`}
                     >
-                      <item.icon
-                        className="h-6 w-6 shrink-0 bg-HoverColor"
-                        aria-hidden="true"
-                      />
+                      <item.icon aria-hidden="true" />
                       {item.name}
                     </a>
                   </li>
