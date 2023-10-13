1ef3f920b3a768bb4cf658f1dc94fb5eb3f2de9a
Michael Saa Olu Siyanbola Johnson <113233530+SOSJ001@users.noreply.github.com>
2023-10-13 15:50:51 +0000

    Add Product Page completed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,24 @@
 import CreateBrand from "@/components/ajo-custom/Full components/CreateBrand";
 import MainLayout from "./layout/main-layout";
+import Addproduct from "@/components/ajo-custom/Full components/addProduct";
 
  
 export default function Home() {
   return (
     <MainLayout >
-      {/* Content goes here */}
-
-
-      {/* //Create Brand */}
+      <div className=" flex flex-col gap-3">
+         {/* //Create Brand */}
       <CreateBrand/>
 
-      
+      <hr  className="border-2"/>
+
+      {/* Add product*/}
+
+      <Addproduct/>
+
+      <hr  className="border-2"/>
+
+      </div>      
     </MainLayout>
  
   );
