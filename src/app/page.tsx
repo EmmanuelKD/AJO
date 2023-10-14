fa9d1996b5318a7c1d9b66f49933aa7a7b026432
woodfur <138519610+woodfur@users.noreply.github.com>
2023-10-14 14:06:40 +0000

    Some Corrections and configurations

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,169 @@
+"use client";
 import CreateBrand from "@/components/ajo-custom/Full components/CreateBrand";
 import MainLayout from "./layout/main-layout";
 import FeedHome from "@/components/ajo-custom/Full components/FeedHome";
 import Addproduct from "@/components/ajo-custom/Full components/addProduct";
 
+import { useEffect, useState } from "react";
+// HIGHLIGHTSTART-importModules
+import { Web3AuthNoModal } from "@web3auth/no-modal";
+import {
+  WALLET_ADAPTERS,
+  CHAIN_NAMESPACES,
+  SafeEventEmitterProvider,
+  IProvider,
+} from "@web3auth/base";
+import { OpenloginAdapter } from "@web3auth/openlogin-adapter";
+import RPC from "../solanaRPC";
+
+const clientId = "YOUR_WEB3AUTH_CLIENT_ID";
+
 export default function Home() {
+  //importing Web3Auth Functions
+
+  const [web3auth, setWeb3auth] = useState<Web3AuthNoModal | null>(null);
+  const [provider, setProvider] = useState<IProvider | null>(null);
+
+  useEffect(() => {
+    const init = async () => {
+      try {
+        const web3auth = new Web3AuthNoModal({
+          clientId,
+          web3AuthNetwork: "testnet", // mainnet, aqua,  cyan or testnet
+          chainConfig: {
+            chainNamespace: CHAIN_NAMESPACES.SOLANA,
+            chainId: "0x1", // Please use 0x1 for Mainnet, 0x2 for Testnet, 0x3 for Devnet
+            rpcTarget: "https://rpc.ankr.com/solana", // This is the public RPC we have added, please pass on your own endpoint while creating an app
+          },
+        });
+
+        const openloginAdapter = new OpenloginAdapter({
+          adapterSettings: {
+            whiteLabel: {
+              appName: "Ajo",
+              logoLight: "https://web3auth.io/images/w3a-L-Favicon-1.svg",
+              logoDark: "https://web3auth.io/images/w3a-D-Favicon-1.svg",
+              defaultLanguage: "en",
+              mode: "dark", // whether to enable dark mode. defaultValue: false
+            },
+          },
+        });
+        web3auth.configureAdapter(openloginAdapter);
+
+        setWeb3auth(web3auth);
+
+        await web3auth.init();
+        if (web3auth.provider) {
+          setProvider(web3auth.provider);
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    init();
+  }, []);
+
+  const login = async () => {
+    if (!web3auth) {
+      console.log("web3auth not initialized yet");
+      return;
+    }
+    const web3authProvider = await web3auth.connectTo(
+      WALLET_ADAPTERS.OPENLOGIN,
+      {
+        mfaLevel: "default", // Pass on the mfa level of your choice: default, optional, mandatory, none
+        loginProvider: "google", // Pass on the login provider of your choice: google, facebook, discord, twitch, twitter, github, linkedin, apple, etc.
+      }
+    );
+    setProvider(web3authProvider);
+  };
+
+  const authenticateUser = async () => {
+    if (!web3auth) {
+      console.log("web3auth not initialized yet");
+      return;
+    }
+    const idToken = await web3auth.authenticateUser();
+    console.log(idToken);
+  };
+
+  const getUserInfo = async () => {
+    if (!web3auth) {
+      console.log("web3auth not initialized yet");
+      return;
+    }
+    const user = await web3auth.getUserInfo();
+    console.log(user);
+  };
+
+  const logout = async () => {
+    if (!web3auth) {
+      console.log("web3auth not initialized yet");
+      return;
+    }
+    await web3auth.logout();
+    setProvider(null);
+  };
+
+  const getAccounts = async () => {
+    if (!provider) {
+      console.log("provider not initialized yet");
+      return;
+    }
+    const rpc = new RPC(provider);
+    const address = await rpc.getAccounts();
+    console.log(address);
+  };
+
+  const getBalance = async () => {
+    if (!provider) {
+      console.log("provider not initialized yet");
+      return;
+    }
+    const rpc = new RPC(provider);
+    const balance = await rpc.getBalance();
+    console.log(balance);
+  };
+
+  const sendTransaction = async () => {
+    if (!provider) {
+      console.log("provider not initialized yet");
+      return;
+    }
+    const rpc = new RPC(provider);
+    const receipt = await rpc.sendTransaction();
+    console.log(receipt);
+  };
+
+  const signMessage = async () => {
+    if (!provider) {
+      console.log("provider not initialized yet");
+      return;
+    }
+    const rpc = new RPC(provider);
+    const signedMessage = await rpc.signMessage();
+    console.log(signedMessage);
+  };
+
+  const getPrivateKey = async () => {
+    if (!provider) {
+      console.log("provider not initialized yet");
+      return;
+    }
+    const rpc = new RPC(provider);
+    const privateKey = await rpc.getPrivateKey();
+    console.log(privateKey);
+  };
+
   return (
     <MainLayout>
       {/* The Div below is for the full components */}
-      <div>
-        <FeedHome />
-      </div>
       <div className=" flex flex-col gap-3">
         {/* //Create Brand */}
+        <br />
+        <br />
+
         <CreateBrand />
 
         <hr className="border-2" />
--- src/components/ajo-custom/Full components/FeedHome.tsx
+++ src/components/ajo-custom/Full components/FeedHome.tsx
@@ -1,19 +1,14 @@
-import React from 'react'
-import HomeTabs from '../home-tabs'
-import TextInput from '../text-input';
-import ComposeBox from '../compose-box';
+import React from "react";
+import HomeTabs from "../home-tabs";
+import TextInput from "../text-input";
+import ComposeBox from "../compose-box";
 
 const FeedHome = () => {
   return (
     <div>
       <HomeTabs />
-      <TextInput />
-      <ComposeBox />
-
-
-
     </div>
-  )
-}
+  );
+};
 
 export default FeedHome;
